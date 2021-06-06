f9698b3e2117a81167fcbb76a00455ad7bb15aa4
wlyh514 <57304851+wlyh514@users.noreply.github.com>
2021-06-06 12:12:42 +0800

    Create db.js

---
+++ code/EmpowerUAfrica/db.js
@@ -0,0 +1,40 @@
+
+
+const db = {
+
+    /*
+        params:
+            - username: String
+            - email: String
+            - password: String
+            - type: int
+            - firstname: String
+            - lastname: String (optional, counld be undefined, if so don't put it into the table.)
+        returns:
+            nothing
+        
+        Creates a new account eneity in the account table.
+    */
+    createNewAccount: (username, email, password, type, firstname, lastname) => {
+
+    }, 
+
+    /*
+        params:
+            - idtype: "username" or "email", indicates the type of id.
+            - id: String, could be an email or a username, depends on idtype. 
+            - password: String
+        
+        returns:
+            - true, if the credentials match
+            - false o\w
+    */
+    signInCredentialsMatch: (idtype, id, password) => {
+
+    }
+
+}; 
+
+
+
+module.exports = db; 
