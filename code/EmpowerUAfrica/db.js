25fab7502a91c6a205ec6cab5dc26c357d34f2cb
YuanqianFang <richardfang001105@gmail.com>
2021-06-07 02:10:48 -0400

    Update db.js
    copy the code from develop branch

--- code/EmpowerUAfrica/db.js
+++ code/EmpowerUAfrica/db.js
@@ -1,3 +1,31 @@
+const mysql = require('mysql2/promise'); 
+const fs = require('fs');
+
+
+const port = '3306';
+const host = 'localhost';
+const database = 'EmpowerUAfricaDB';
+let connection;
+
+// Read MySQL cridentials
+fs.readFile("./MySQLCredentials.json", async (err, data) => {
+    if (err) {
+        throw err;
+    }
+    let obj = JSON.parse(data); 
+    user = obj.user; 
+    password = obj.password; 
+
+    // Establish connection to MySQL
+    connection = await mysql.createConnection({
+        host, 
+        user,
+        password, 
+        port,
+        database
+    });
+    console.log(`Connected to MySQL ${user}@${host}, database ${database}`);
+});
 
 
 const db = {
@@ -15,8 +43,11 @@ const db = {
         
         Creates a new account eneity in the account table.
     */
-    createNewAccount: (username, email, password, type, firstname, lastname) => {
-
+    createNewAccount: async (username, email, password, type, firstname, lastname) => {
+        let sql = 'INSERT INTO Login(username, email, password, type, first_name, last_name)\
+        VALUES(?, ?, ?, ?, ?, ?);'; 
+        let data = [username, email, password, type, firstname, lastname]; 
+        await connection.execute(sql, data); 
     }, 
 
     /*
@@ -29,12 +60,15 @@ const db = {
             - true, if the credentials match
             - false o\w
     */
-    signInCredentialsMatch: (idtype, id, password) => {
+    credentialsMatch: async (idtype, id, password) => {
+        let sql = `SELECT password FROM Login WHERE ${idtype} = ?`; 
+        let data = [id]; 
 
+        let actualPasswd = (await connection.execute(sql, data))[0][0].password; 
+        return actualPasswd === password; 
     }
 
 }; 
-
 
 
 module.exports = db; 
