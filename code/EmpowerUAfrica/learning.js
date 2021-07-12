1d97490ed1a305821559ed93f035a51efab7665b
ljz3 <kevin.ottawa@hotmail.com>
2021-07-11 21:36:09 -0400

    [UOF-111] Completed getCourses Endpoint

--- code/EmpowerUAfrica/learning.js
+++ code/EmpowerUAfrica/learning.js
@@ -1,6 +1,32 @@
+const express = require('express');
+
 const express = require('express'); 
+const fs = require('fs').promises; 
+
+const db = require('./db'); 
+
+const utils = require('./utils');
+const validation = require('./validation');
+const admin = require('./admin'); 
 
 const router = express.Router(); 
+
+/* 
+    Endpoint to get all information of all courses
+    Params: None
+*/
+router.get('/getCourses', async (req, res) => {
+
+    let results = await db.getCourses();
+    if(results != null){
+        res.status(200).json(results);
+    }else{
+        res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+
+});
 
 
 module.exports = router; 
